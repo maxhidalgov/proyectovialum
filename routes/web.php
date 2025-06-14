521e51c26d7295150c7f9a73f06353067beb14e1
maxhidalgov <maxxhidalgo@gmail.com>
2025-06-14 12:56:20 -0400

    Rutas simplificadas para devolver index.html correctamente

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CotizacionController;
-use App\Http\Controllers\ImportacionController;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/cotizaciones/{id}/pdf', [CotizacionController::class, 'generarPDF']);
-
-
-Route::get('/importar-productos', [ImportacionController::class, 'importarProductos']);
-Route::get('/importar-producto-color-proveedor', [ImportacionController::class, 'importarProductoColorProveedor']);
-
+Route::get('/debug-index', function () {
+    return response()->file(public_path('index.html'));
+});
 
 Route::get('/{any}', function () {
     return File::get(public_path('index.html'));
