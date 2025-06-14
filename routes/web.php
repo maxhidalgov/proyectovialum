a791c3f46a4076ea6b50c720f4264f5cadf8fafb
maxhidalgov <maxxhidalgo@gmail.com>
2025-06-14 12:27:11 -0400

    Corrijo conflicto de ruta raíz con index.html

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CotizacionController;
 use App\Http\Controllers\ImportacionController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\File;
 
 Route::get('/cotizaciones/{id}/pdf', [CotizacionController::class, 'generarPDF']);
 
 
 Route::get('/importar-productos', [ImportacionController::class, 'importarProductos']);
 Route::get('/importar-producto-color-proveedor', [ImportacionController::class, 'importarProductoColorProveedor']);
+
+Route::get('/{any}', function () {
+    return File::get(public_path('index.html'));
+})->where('any', '.*');
