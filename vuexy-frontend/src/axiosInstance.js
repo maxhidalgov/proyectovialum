63e45201cdf7d2cc164ff6b4e752fb48a36e6886
maxhidalgov <maxxhidalgo@gmail.com>
2025-06-19 18:37:37 -0400

    cambio el axiosinstance.js para apuntar al host de railway

--- vuexy-frontend/src/axiosInstance.js
+++ vuexy-frontend/src/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // Ajusta si usas otra URL
+  baseURL: 'https://proyectovialum-production.up.railway.app', // Ajusta si usas otra URL
 })
 
 export default api
