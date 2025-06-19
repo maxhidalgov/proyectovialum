574248278127e2d34a85e1bb9e640a6bdfbc6827
maxhidalgov <maxxhidalgo@gmail.com>
2025-06-19 19:26:44 -0400

    agregar un usuario3

--- database/seeders/data/proyectovialum_max.sql
+++ database/seeders/data/proyectovialum_max.sql
@@ -1,3 +1,3 @@
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(5, 'Max Hidalgo', 'max.hidalgo@vialum.cl', '$2y$10$9UOpTiw8nmQszzPyUP9NuubRn4gEjHOgCR2O/WSUROXDf/qz4reG2', NOW(), NOW());
-
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`)
+VALUES
+(1, 'Juan Pérez', 'juan.perez@vialum.cl', '$2y$10$9UOpTiw8nmQszzPyUP9NuubRn4gEjHOgCR2O/WSUROXDf/qz4reG2', NOW(), NOW());
