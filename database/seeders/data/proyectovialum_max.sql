e08c6c245a98529d606c4861e95456e7cea51c79
maxhidalgo <maxxhidalgo@gmail.com>
2025-06-20 23:46:43 -0400

    poblar tabla unidades id 4

--- database/seeders/data/proyectovialum_max.sql
+++ database/seeders/data/proyectovialum_max.sql
@@ -1,5 +1,2 @@
 INSERT INTO `unidades` (`id`, `nombre`, `requiere_division`, `descripcion`, `created_at`, `updated_at`) VALUES
-(1, 'unidad', 0, 'Producto individual, no requiere división', '2025-03-22 17:55:19', '2025-03-22 17:55:19'),
-(2, 'barra', 1, 'Producto en barras largas que se divide según largo', '2025-03-22 17:55:19', '2025-03-22 17:55:19'),
-(3, 'metro', 0, 'Producto calculado directamente por metro lineal', '2025-03-22 17:55:19', '2025-03-22 17:55:19'),
 (4, 'm2', 0, 'metro cuadrado para cristales', '2025-03-26 15:08:20', '2025-03-26 15:08:28');
