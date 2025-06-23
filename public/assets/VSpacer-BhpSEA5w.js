f8506159727413c69d44f0c1495d900e5f05c574
maxhidalgov <maxxhidalgo@gmail.com>
2025-06-23 13:14:42 -0400

    conectando login con bd2

---
+++ public/assets/VSpacer-BhpSEA5w.js
@@ -0,0 +1,1 @@
+/* empty css              */import{c as r}from"./VAvatar-CEYI93iM.js";const e=r("v-spacer","div","VSpacer");export{e as V};
--- vuexy-frontend/src/axiosInstance.js
+++ vuexy-frontend/src/axiosInstance.js
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: 'https://proyectovialum-production.up.railway.app', // Ajusta si usas otra URL
 })
 
-instance.interceptors.request.use(config => {
+api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
